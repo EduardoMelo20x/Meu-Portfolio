3c73ed0f3bd19154dcbd56913f7eaa3cdc93c3fc
Eduardo Melo dos santos <eduardomelo20x@gmail.com>
2024-10-15 01:44:24 -0300

    Update

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+
 var typed = new Typed('#typed-text', {
     strings: ["Desenvolvedor de Software"],
     typeSpeed: 100,
     backSpeed: 50,
     backDelay: 2000,
-    loop: true
+    loop: true,
+    cursorChar: '|', // Adiciona um cursor
+    showCursor: true // Mostra o cursor
  }); 
